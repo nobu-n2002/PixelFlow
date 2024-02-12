fa75d107506db5576e3b12c2b1089bac9b4a9668
Nobuto Nakamichi <74840706+nobu-n2002@users.noreply.github.com>
2024-02-12 19:25:56 +0900

    Update build3d.sh

--- scripts/build3d.sh
+++ scripts/build3d.sh
@@ -7,13 +7,14 @@ FC_FLAG='-O3 -fopenmp -fno-automatic -o'
 BIN_DIR=bin
 EXE=ibm3
 
-mkdir -p bin
+mkdir -p ${BIN_DIR}
+cd ${BIN_DIR}
 
 rm -f ${BIN_DIR}/*.o ${BIN_DIR}/*.mod ${BIN_DIR}/*.exe ${BIN_DIR}/*.out ${BIN_DIR}/ibm3
 
-echo ${FC} ${FC_FLAG} ${BIN_DIR}/${EXE} ${SRC_DIR}/${SRC}
+echo ${FC} ${FC_FLAG} ${EXE} ../${SRC_DIR}/${SRC}
 
-if ${FC} ${FC_FLAG} ${BIN_DIR}/${EXE} ${SRC_DIR}/${SRC}; then
+if ${FC} ${FC_FLAG} ${EXE} ../${SRC_DIR}/${SRC}; then
     echo "Build complete. Executable: bin/ibm3"
 else
     echo "Error: Build failed."
