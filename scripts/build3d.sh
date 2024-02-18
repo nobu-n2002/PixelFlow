6cf82e14cfe0bf39e7d480ccfe16dcaf37882129
Nobuto Nakamichi <74840706+nobu-n2002@users.noreply.github.com>
2024-02-19 00:18:33 +0900

    Update build3d.sh

--- scripts/build3d.sh
+++ scripts/build3d.sh
@@ -5,16 +5,17 @@ SRC=ibm_3d.f90
 FC=gfortran
 FC_FLAG='-O3 -fopenmp -fno-automatic -o'
 BIN_DIR=bin
-EXE=ibm3
+EXE=ibm2
 
 mkdir -p bin
+cd bin
 
-rm -f ${BIN_DIR}/*.o ${BIN_DIR}/*.mod ${BIN_DIR}/*.exe ${BIN_DIR}/*.out ${BIN_DIR}/ibm3
+rm -f *.o *.mod *.exe *.out ibm2
 
 echo ${FC} ${FC_FLAG} ${BIN_DIR}/${EXE} ${SRC_DIR}/${SRC}
 
-if ${FC} ${FC_FLAG} ${BIN_DIR}/${EXE} ${SRC_DIR}/${SRC}; then
-    echo "Build complete. Executable: bin/ibm3"
+if ${FC} ${FC_FLAG} ${EXE} ../${SRC_DIR}/${SRC}; then
+    echo "Build complete. Executable: bin/ibm2"
 else
     echo "Error: Build failed."
 fi
