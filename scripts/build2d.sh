2e93ab7a4937f13a0034e3eecb30a970bebcdda4
Nobuto Nakamichi <74840706+nobu-n2002@users.noreply.github.com>
2024-02-23 01:46:16 +0900

    Update build2d.sh

--- scripts/build2d.sh
+++ scripts/build2d.sh
@@ -7,12 +7,16 @@ FC_FLAG='-O3 -fopenmp -fno-automatic -o'
 BIN_DIR=bin
 EXE=ibm2
 
-mkdir -p bin
-cd bin
+echo mkdir -p ${BIN_DIR}
+echo cd ${BIN_DIR}
 
+mkdir -p ${BIN_DIR}
+cd ${BIN_DIR}
+
+echo rm -f *.o *.mod *.exe *.out ibm2
 rm -f *.o *.mod *.exe *.out ibm2
 
-echo ${FC} ${FC_FLAG} ${BIN_DIR}/${EXE} ${SRC_DIR}/${SRC}
+echo ${FC} ${FC_FLAG} ${EXE} ../${SRC_DIR}/${SRC}
 
 if ${FC} ${FC_FLAG} ${EXE} ../${SRC_DIR}/${SRC}; then
     echo "Build complete. Executable: bin/ibm2"
